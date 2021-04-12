ddea3f31d1adfe41db6ee159bdf98d1abe229a60
rciez2125 <rebecca.ciez@gmail.com>
2021-04-12 13:17:53 -0400

    add pandas
    updates to include pandas analysis

--- SampleSheet.py
+++ SampleSheet.py
@@ -1,17 +1,29 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
 creds = ServiceAccountCredentials.from_json_keyfile_name("testSheets-62f6c09b9d0d.json", scope)
 
-client = gspread.auhorize(creds)
+#client = gspread.auhorize(creds)
 
-sheet = client.open("sample sheet").sheet1
+#sheet = client.open("sample sheet").sheet1
 
-data = sheet.get_all_records()
+#data = sheet.get_all_records()
 
-row = sheet.row_values(3)
-col = sheet.col_values(3)
+#row = sheet.row_values(3)
+#col = sheet.col_values(3)
 
-pprint(col)
+#pprint(col)
+
+gc = gspread.authorize(creds)
+
+wks = gc.open("sample sheet").sheet1
+
+data = wks.get_all_values()
+headers = data.pop(0)
+
+df = pd.DataFrame(data, columns=headers)
+
+print(df)
