870dba54216630393e5448d505c9b1b6e1cce56f
Pratik260199 <78764501+Pratik260199@users.noreply.github.com>
2021-05-31 16:55:12 -0400

    Update Battery_System_Components.py

--- Battery_System_Components.py
+++ Battery_System_Components.py
@@ -1,7 +1,30 @@
+# importing the required libraries
+import gspread
 import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+from googleapiclient.discovery import build
 
-sheet_id = '1izazCry301klvetVCTs9cxpSZgSI671jXkG3dRcfnoI'
+# define the scope
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
-df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
-# df=pd.DataFrame(data)
+# add credentials to the account
+creds = ServiceAccountCredentials.from_json_keyfile_name('bat-sys-comp-7e968489ad5d.json', scope)
+
+# authorize the clientsheet
+client = gspread.authorize(creds)
+
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
+SPREADSHEET_ID = '1izazCry301klvetVCTs9cxpSZgSI671jXkG3dRcfnoI'
+DATA_TO_PULL = 'Lists!A2:E8'
+service = build('sheets', 'v4', credentials=creds)
+sheet = service.spreadsheets()
+
+rows = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=DATA_TO_PULL).execute()
+data = rows.get('values')
+
+def pull_sheet_data(SCOPES,SPREADSHEET_ID,DATA_TO_PULL):
+
+    data = pull_sheet_data(SCOPES,SPREADSHEET_ID,DATA_TO_PULL)
+df = pd.DataFrame(data[0:7], columns=data[0])
 print(df)
