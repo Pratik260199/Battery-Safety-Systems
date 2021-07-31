6a6399d338743f491afd60f14c28c0eeb1f3c72c
rciez2125 <rebecca.ciez@gmail.com>
2021-07-31 09:06:11 -0400

    update cost calculations

--- Calculations.py
+++ Calculations.py
@@ -3,71 +3,29 @@ import gspread
 import pandas as pd
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
+import numpy as np
 
 import ReadSheet as read
 
 
-def define_sheet_data():  # Make modular w/ 'sheet' input variable
-    # Defining the scope
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-
-    # Adding credentials to the account; Make sure to change to your .json key
-    creds = Credentials.from_service_account_file('me-summer-project-61b3f64d4f79.json')
-
-    # Authorizing the clientsheet
-    client = gspread.authorize(creds)
-
-    # specifying the sheet details
-    SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-    SPREADSHEET_ID = '1izazCry301klvetVCTs9cxpSZgSI671jXkG3dRcfnoI'
-    DATA_TO_PULL = 'Battery_System_Components'  # Put 'sheet' input variable here
-    service = build('sheets', 'v4', credentials=creds)
-    sheet = service.spreadsheets()
-
-    rows = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=DATA_TO_PULL).execute()
-    data = rows.get('values')
-
-    return (data)
-
-
-# Loops through all data on specified sheet. Records data until a break
-# between two sections is reached and uses recorded data to create a new
-# dataframe. Clears recorded data. Repeats process until end of sheet.
-# Returns list of dataframes
-def create_dataframes(data):
-    df = []  # List for whole dataframe
-    section = []  # List for individual sections
-
-    for i in data:
-        if i == [] or i == data[-1]:  # Indicates breaks between sections or end
-            if section == []:
-                pass
-            else:
-                if i == data[-1]:  # Needed to add last line of last section
-                    section.append(i)
-
-                    # section[0] should always be the label from spreadsheet
-                df.append(pd.DataFrame(section[1:len(section)], columns=section[0])
-                          .set_index("Select a Configuration", drop=True))
-                section = []
-        else:
-            section.append(i)
-
-    return (df)
 
 ################################################### Cost Calculations ######################################################
 # Cost Total for Cell Components
-dataframes = create_dataframes(define_sheet_data())
-df0 = dataframes[0]
-df0['Cost (USD)'] = pd.to_numeric(df0['Cost (USD)'], errors='coerce')  # Converting every cost value to numeric
-df0['Cost (USD)'] = df0['Cost (USD)'].fillna(0)  # Replacing NaN or string values with zero
-temp_val = df0['Cost (USD)']
+components = read.create_dataframes(read.define_sheet_data('Battery_System_Components'), "Select a Configuration")
+
+df0 = components[0]
+print(df0)
+#dataframes = create_dataframes(define_sheet_data())
+#df0 = dataframes[0]
+df0['Total Cost (USD)'] = pd.to_numeric(df0['Total Cost (USD)'], errors='coerce')  # Converting every cost value to numeric
+df0['Total Cost (USD)'] = df0['Total Cost (USD)'].fillna(0)  # Replacing NaN or string values with zero
+print(sum(df0['Total Cost (USD)']))
+temp_val = df0['Total Cost (USD)']
 maxsumcost0 = sum(temp_val.values)
 print('Total estimated cost (USD) for Cells:', maxsumcost0)
 
 # Cost Total for Module Components
-dataframes = create_dataframes(define_sheet_data())
-df1 = dataframes[1]
+df1 = components[1]
 df1['Cost (USD)'] = pd.to_numeric(df1['Cost (USD)'], errors='coerce')  # Converting every cost value to numeric
 df1['Cost (USD)'] = df1['Cost (USD)'].fillna(0)  # Replacing NaN or string values with zero
 temp_val = df1['Cost (USD)']
@@ -75,8 +33,7 @@ maxsumcost1 = sum(temp_val.values)
 print('Total estimated cost (USD) for Modules:', maxsumcost1)
 
 # Cost Total for Rack Components
-dataframes = create_dataframes(define_sheet_data())
-df2 = dataframes[2]
+df2 = components[2]
 df2['Cost (USD)'] = pd.to_numeric(df2['Cost (USD)'], errors='coerce')  # Converting every cost value to numeric
 df2['Cost (USD)'] = df2['Cost (USD)'].fillna(0)  # Replacing NaN or string values with zero
 temp_val = df2['Cost (USD)']
@@ -84,8 +41,7 @@ maxsumcost2 = sum(temp_val.values)
 print('Total estimated cost (USD) for Racks:', maxsumcost2)
 
 # Cost Total for Housing Components
-dataframes = create_dataframes(define_sheet_data())
-df3 = dataframes[3]
+df3 = components[3]
 df3['Cost (USD)'] = pd.to_numeric(df3['Cost (USD)'], errors='coerce')  # Converting every cost value to numeric
 df3['Cost (USD)'] = df3['Cost (USD)'].fillna(0)  # Replacing NaN or string values with zero
 temp_val = df3['Cost (USD)']
@@ -93,10 +49,10 @@ maxsumcost3 = sum(temp_val.values)
 print('Total estimated cost (USD) for Housing:', maxsumcost3)
 
 # Total cost calculation
-cell = dataframes[0]
-module = dataframes[1]
-rack = dataframes[2]
-housing = dataframes[3]
+cell = components[0]
+module = components[1]
+rack = components[2]
+housing = components[3]
 cellpermod = float(module.loc['Cells', 'Number per module'])
 modperrack = float(rack.loc['Modules', 'Number per rack'])
 rackperhousing = float(housing.loc['Racks', 'Number'])
@@ -106,8 +62,7 @@ print('Total estimated cost (USD) for system:', totalcost)
 
 ################################################### Weight Calculations ######################################################
 # Weight Total for Rack Components
-dataframes = create_dataframes(define_sheet_data())
-df1 = dataframes[1]
+df1 = components[2]
 df1['Weight (kg)'] = pd.to_numeric(df1['Weight (kg)'], errors='coerce')  # Converting every cost value to numeric
 df1['Weight (kg)'] = df1['Weight (kg)'].fillna(0)  # Replacing NaN or string values with zero
 temp_val = df1['Weight (kg)']
@@ -115,8 +70,7 @@ maxsumweight1 = sum(temp_val.values)
 print('Total estimated weight (kg) for Modules:', maxsumweight1)
 
 # Weight Total for Module Components
-dataframes = create_dataframes(define_sheet_data())
-df2 = dataframes[2]
+df2 = components[1]
 df2['Weight (kg)'] = pd.to_numeric(df2['Weight (kg)'], errors='coerce')  # Converting every cost value to numeric
 df2['Weight (kg)'] = df2['Weight (kg)'].fillna(0)  # Replacing NaN or string values with zero
 temp_val = df2['Weight (kg)']
@@ -124,8 +78,7 @@ maxsumweight2 = sum(temp_val.values)
 print('Total estimated weight (kg) for Racks:', maxsumweight2)
 
 # Weight Total for Housing Components
-dataframes = create_dataframes(define_sheet_data())
-df3 = dataframes[3]
+df3 = components[3]
 df3['Weight (kg)'] = pd.to_numeric(df3['Weight (kg)'], errors='coerce')  # Converting every cost value to numeric
 df3['Weight (kg)'] = df3['Weight (kg)'].fillna(0)  # Replacing NaN or string values with zero
 temp_val = df3['Weight (kg)']
@@ -148,31 +101,30 @@ components = read.create_dataframes(read.define_sheet_data('Battery_System_Compo
 cells = read.find_num(module, module.index[2], 'Number per module')
 modules = read.find_num(rack, rack.index[0], 'Number per rack')
 racks = read.find_num(housing, housing.index[0], 'Number')
+print(cells, modules, racks)
+
 
 AnodeCapacity = read.find_num(cell, cell.index[2], 'Total Capacity [Ah]')*read.find_num(cell, cell.index[8], 'Nominal Voltage (V)')
 CathodeCapacity = read.find_num(cell, cell.index[5], 'Total Capacity [Ah]')*read.find_num(cell, cell.index[8], 'Nominal Voltage (V)')
+print(AnodeCapacity, CathodeCapacity)
+cellcapacity = min(AnodeCapacity, CathodeCapacity)
 
-if AnodeCapacity < CathodeCapacity:
-    cellcapacity = AnodeCapacity
-else: cellcapacity = CathodeCapacity
 
-totalenergy = cellcapacity*cells*modules*racks
+totalenergy = cellcapacity*cells*modules*racks/1000
 print(totalenergy)
-
+print(totalcost/(totalenergy))
 hoursdischarge = 4   #Convert this to pull data from the experiment we are running #Pull from experiment
 storageDuration = 2  #Number of hours are variable - case by case basis #Pull from experiment
 eta_RTE = 0.81   #Conservative estimate
 eta_discharge = 0.9
 eta_charge = 0.9
 
-capX_energy = totalcost/totalenergy       #How much energy is being produced #This is totalcost/(capacity of 1 cell*No of cells per module*No of modules per rack*No of racks)
-ratedPower = capX_energy/hoursdischarge  #Sys energy capacity/hours of discharge
+capX_energy = totalcost/totalenergy
+ratedPower = 1/hoursdischarge
 capX_power = 0
-OM = 0.02*totalcost
+OM = 0.02
 elecPrice = 0.025 #per kWH
 
 from lcosScripts import calculateLCOS
 LCOS = calculateLCOS(ratedPower, storageDuration, eta_RTE, eta_discharge, eta_charge, capX_energy, capX_power, OM, elecPrice)
 print(LCOS)
-
-
--- lcosScripts.py
+++ lcosScripts.py
@@ -1,7 +1,7 @@
-# short pieces of code to do quick data processing and calculations as part of MOST DAYS technoeconomic analysis 
+# short pieces of code to do quick data processing and calculations as part of MOST DAYS technoeconomic analysis
 
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 import csv
 import matplotlib
 matplotlib.use('TkAgg')
@@ -9,16 +9,16 @@ import matplotlib.pyplot as plt
 import math
 
 def calculateLCOS(ratedPower, storageDuration, eta_RTE, eta_discharge, eta_charge, capX_energy, capX_power, OM, elecPrice):
-    # this list of parameters was specified by ARPA-E in their DAYS FOA 
-  
-    dutyProfile = pd.read_csv('Inputs/daysDutyProfile.csv')
+    # this list of parameters was specified by ARPA-E in their DAYS FOA
+
+    dutyProfile = pd.read_csv('daysDutyProfile.csv')
     discountRate = 0.1 # 10%
-    systemLifetime = 10 # was 20 # years
-    discountFactors = 1/((1+discountRate)**np.arange(21)) # annual discount factors based on specified rate
+    systemLifetime = 20 # was 20 # years
+    discountFactors = 1/((1+discountRate)**np.arange(systemLifetime+1)) # annual discount factors based on specified rate
     # pull annual electricity delivered based on system power & storage duration
     elecDelivered = dutyProfile[:int(storageDuration)]
-    elecDelivered = sum(elecDelivered.cycles*ratedPower) 
-    
+    elecDelivered = sum(elecDelivered.cycles*ratedPower)
+
     fullCycleEquivalent = elecDelivered / (ratedPower * storageDuration)
     capX = capX_energy / eta_discharge + capX_power / storageDuration
     elecCost = ((1 / eta_RTE) - 1) * fullCycleEquivalent * elecPrice
