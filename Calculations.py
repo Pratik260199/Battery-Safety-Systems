a91b73ecd54c8c02da37ab9f21080067d57307a7
chakra24 <chakra24@purdue.edu>
2021-07-31 14:38:46 -0400

    updated weight calculations (same format as cost total calculations)

--- Calculations.py
+++ Calculations.py
@@ -7,8 +7,6 @@ import numpy as np
 
 import ReadSheet as read
 
-
-
 ################################################### Cost Calculations ######################################################
 # Cost Total for Cell Components
 components = read.create_dataframes(read.define_sheet_data('Battery_System_Components'), "Select a Configuration")
@@ -61,32 +59,35 @@ print('Total estimated cost (USD) for system:', totalcost)
 
 
 ################################################### Weight Calculations ######################################################
-# Weight Total for Rack Components
-df1 = components[2]
-df1['Weight (kg)'] = pd.to_numeric(df1['Weight (kg)'], errors='coerce')  # Converting every cost value to numeric
-df1['Weight (kg)'] = df1['Weight (kg)'].fillna(0)  # Replacing NaN or string values with zero
-temp_val = df1['Weight (kg)']
+# Weight Total for Module Components
+df1 = components[1]
+df1['Total Weight (kg)'] = pd.to_numeric(df1['Total Weight (kg)'], errors='coerce')  # Converting every cost value to numeric
+df1['Total Weight (kg)'] = df1['Total Weight (kg)'].fillna(0)  # Replacing NaN or string values with zero
+temp_val = df1['Total Weight (kg)']
 maxsumweight1 = sum(temp_val.values)
 print('Total estimated weight (kg) for Modules:', maxsumweight1)
 
-# Weight Total for Module Components
-df2 = components[1]
-df2['Weight (kg)'] = pd.to_numeric(df2['Weight (kg)'], errors='coerce')  # Converting every cost value to numeric
-df2['Weight (kg)'] = df2['Weight (kg)'].fillna(0)  # Replacing NaN or string values with zero
-temp_val = df2['Weight (kg)']
+# Weight Total for Rack Components
+df2 = components[2]
+df2['Total Weight (kg)'] = pd.to_numeric(df2['Total Weight (kg)'], errors='coerce')  # Converting every cost value to numeric
+df2['Total Weight (kg)'] = df2['Total Weight (kg)'].fillna(0)  # Replacing NaN or string values with zero
+temp_val = df2['Total Weight (kg)']
 maxsumweight2 = sum(temp_val.values)
 print('Total estimated weight (kg) for Racks:', maxsumweight2)
 
 # Weight Total for Housing Components
 df3 = components[3]
-df3['Weight (kg)'] = pd.to_numeric(df3['Weight (kg)'], errors='coerce')  # Converting every cost value to numeric
-df3['Weight (kg)'] = df3['Weight (kg)'].fillna(0)  # Replacing NaN or string values with zero
-temp_val = df3['Weight (kg)']
+df3['Total Weight (kg)'] = pd.to_numeric(df3['Total Weight (kg)'], errors='coerce')  # Converting every cost value to numeric
+df3['Total Weight (kg)'] = df3['Total Weight (kg)'].fillna(0)  # Replacing NaN or string values with zero
+temp_val = df3['Total Weight (kg)']
 maxsumweight3 = sum(temp_val.values)
 print('Total estimated weight (kg) for Housing:', maxsumweight3)
 
-totalweight = maxsumweight1 + maxsumweight2 + maxsumweight3
-print('Total estimated weight (kg) of the system', totalweight)
+
+# Total weight calculation
+
+totalweight = (maxsumweight1*modperrack*rackperhousing) + (maxsumweight2*rackperhousing) + maxsumweight3
+print('Total estimated weight (kg) for system:', totalweight)
 
 '''
 # Example for implementing in code
